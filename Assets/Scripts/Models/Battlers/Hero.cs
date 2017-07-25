fcb722ecb4b31fbd7579a969aed53ea1fd31a68d
Yoshikawa Naohiro <yoshikawa@oniongames.jp>
2017-07-25 17:10:42 +0900

    待機中にHPが回復する要素を追加

--- Assets/Scripts/Models/Battlers/Hero.cs
+++ Assets/Scripts/Models/Battlers/Hero.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using UniRx;
+using System;
 
 namespace ProcedualLevels.Models
 {
@@ -15,6 +16,14 @@ namespace ProcedualLevels.Models
                 x.Hp.Value -= Attack.Value;
                 view.Knockback(this, x, x.Attack.Value);
                 view.Knockback(x, this, Attack.Value);
+            });
+            Observable.Interval(TimeSpan.FromMilliseconds(500))
+                      .Skip(2)
+                      .TakeUntil(view.BattleObservable)
+                      .Repeat()
+                      .Subscribe(x =>
+            {
+                Hp.Value += 1;
             });
         }
     }
